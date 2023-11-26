c81dd639958733b6afc7741904ae736cf3c951a4
Shira Cohen <shira140@gmail.com>
2023-11-26 14:38:11 +0200

    fonts

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -2,9 +2,11 @@
 import logo from '../../public/logo.png'
 import menu from './icons/menu.svg'
 import close from './icons/close.svg'
+import hero from './img/hero.jpg'
 
 export {
     logo,
     menu,
-    close
+    close,
+    hero
 }
--- frontend/src/cmps/Hero.jsx
+++ frontend/src/cmps/Hero.jsx
@@ -1,13 +1,20 @@
-
+import { hero } from "../assets" 
 
 const Hero = () => {
 
     return (
         <div className="hero-container">
-            <div className="hero-bcg"></div>
+            <div className="hero-bcg">
+                <img src={hero} alt="hero" />
+            </div>
             <div className="hero-txt">
-                <br/>
-                HERO TEXT
+                <div>
+                    MIA GROUP
+                </div>
+                <div>
+                    יזמות ובנייה
+                </div>
+
             </div>
         </div>
 

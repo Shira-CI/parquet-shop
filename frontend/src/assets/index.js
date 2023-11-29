dedb2c3819899d0c00c2782aadca26381b1dd84a
Shira Cohen <shira140@gmail.com>
2023-11-29 15:27:21 +0200

    product index hero

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -3,10 +3,12 @@ import logo from '../../public/logo.png'
 import menu from './icons/menu.svg'
 import close from './icons/close.svg'
 import hero from './img/hero.jpg'
+import parquetHero from './img/parquet-index.jpg'
 
 export {
     logo,
     menu,
     close,
-    hero
+    hero,
+    parquetHero
 }
---
+++ frontend/src/pages/Product.index.jsx
@@ -0,0 +1,21 @@
+import { parquetHero } from "../assets"
+
+const ProductIndex = () => {
+
+    return (
+        <div className="products-index">
+            <div className="products-hero">
+                <div className="products-hero-img">
+                <img src={parquetHero} alt="" />
+                </div>
+                <span >
+                    פרקטים
+                </span>
+
+            </div>
+            ProductIndex
+        </div>
+    )
+}
+
+export default ProductIndex

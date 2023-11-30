61ccd2a454e682c3d777ab99aabbd7b54bc8b6bf
Shira Cohen <shira140@gmail.com>
2023-11-30 12:04:46 +0200

    products list + preview style

--- frontend/src/cmps/product.preview.jsx
+++ frontend/src/cmps/product.preview.jsx
@@ -6,7 +6,7 @@ import { parquetImg } from "../assets"
 export function ProductPreview({ product, onRemoveProduct, user, onAddToCart }) {
 
     return (
-        // <div className="product-preview">
+        <>
             <Link className="details-link" to={`/product/${product._id}`}>
 
                 <div className="preview-img-container">
@@ -15,17 +15,26 @@ export function ProductPreview({ product, onRemoveProduct, user, onAddToCart }) 
 
                 </div>
 
-                <h4>{product.title}</h4>
+                <h2 className="product-preview-title">{product.title}</h2>
+            </Link>
 
-                <p>מחיר: <span> {product.price} </span> למ"ר</p>
-                {/* {!product.inStock && <div className="product-unavailable">
+            <div className="product-price-container">
+                <p className="product-preview-price">  <span> ₪{product.price} </span>
+                </p>
+                <div>
+                למ"ר
+            </div>
+            </div>
+           
+
+            {/* {!product.inStock && <div className="product-unavailable">
                     <span >Out of stock</span>
                 </div>
                 } */}
-            </Link>
 
 
-        // </div>
+        </>
+
     )
 }
 

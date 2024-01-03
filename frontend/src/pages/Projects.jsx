8040462fa005bfbb33167f4bc292a4c65eef7896
Shira Cohen <shira140@gmail.com>
2024-01-03 19:41:05 +0200

    projects page basic layout

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,8 +1,15 @@
 const Projects = () => {
 
     return (
-        <div>
-            Projects
+        <div className="projects-page">
+        <h2>
+            פרויקטים
+        </h2>
+        <section className="projects">
+        <div>פרויקט 1</div>
+        <div>פרויקט 2</div>
+        <div>פרויקט 3</div>
+        </section>
         </div>
     )
 }
